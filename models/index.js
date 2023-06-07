8c20a87f2b01e6acc94a5a48e4a52889bd541913
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-06-07 16:57:17 +1000

    added models

---
+++ models/index.js
@@ -0,0 +1,38 @@
+const Comment = require('./Comment');
+const BlogPost = require('./BlogPost');
+const User = require('./User');
+
+//Relationship between BlogPost and User
+BlogPost.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(BlogPost, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+//Relationship between Comment and User
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+//Relationship between Comment and BlogPost
+Comment.belongsTo(BlogPost, {
+    foreignKey: blog_id
+});
+
+BlogPost, hasMany(Comment, {
+    foreignKey: blog_id
+});
+
+module.exports = {
+    Comment,
+    BlogPost,
+    User
+};
