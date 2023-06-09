e78bc3913bd0c629c13bfb04d7c88932c893a5ad
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-06-09 14:27:46 +1000

    Added bare bones to homepage and blog post displayed

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,2 +1,22 @@
 const router = require('express').Router();
+const { BlogPost } = require('../models');
+
+//GET all blog posts
+router.get("/", async (req, res) => {
+    try {
+        const blogData = await BlogPost.findAll();
+
+        const blogs = blogData.map((blogpost) =>
+            blogpost.get({ plain: true })
+        );
+
+        console.log(blogs)
+
+        res.render("homepage", { blogs });
+    } catch (err) {
+        res.status(500).json(err);
+        console.log(err)
+    }
+});
+
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-// const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./homeRoutes');
 // const dashboardRoutes = require('./dashboardRoutes');
 // const blogPostRoutes = require('./blogPostRoutes');
 
 router.use('/api', apiRoutes);
-// router.use('/', homeRoutes);
+router.use('/', homeRoutes);
 // router.use('/dashboard', dashboardRoutes);
 // router.use('/post', blogPostRoutes);
 
