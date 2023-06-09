be709afa26eb0f34548ae0f47096e6e309b7a171
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-06-09 20:27:56 +1000

    Trying to add ability to add new blog posts

---
+++ public/js/new-post.js
@@ -0,0 +1,38 @@
+const btn = document.querySelector("#create-post");
+
+
+
+btn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const title = document.getElementById("blog-title").value;
+    const content = document.getElementById("blog-content").value;
+
+    console.log(title);
+    console.log(content)
+
+    const user_id = 1;
+
+    fetch('/api/post', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            content,
+            user_id
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Data saved successfully');
+            alert("Post Added")
+        } else {
+            console.error('Failed to save data');
+        }
+    })
+    .catch(error => {
+        console.error('Error', error);
+    })
+});
